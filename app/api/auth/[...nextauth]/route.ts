7721aaf4b5a1b6c4896bbd0313a57efd828a38bb
Adiquatech <quadrikolawoleadisa@gmail.com>
2026-03-25 13:42:11 +0100

    root corrected

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,4 @@
 // src/app/api/auth/[...nextauth]/route.ts
-import { handlers } from '@/auth';   // ← This imports from your auth.ts
+import { handlers } from '@../../../auth';   // ← This imports from your auth.ts
 
 export const { GET, POST } = handlers;
